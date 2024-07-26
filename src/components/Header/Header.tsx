b31ddb11469fde47e2759332b5d4a4d95252f61c
João victor morais mendes <114888951+codingwithvm@users.noreply.github.com>
2024-07-26 21:34:21 +0000

    feat: add Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,27 @@
-import { Logo } from "../Logo/Logo"
-import { SearchBar } from "../SearchBar/SearchBar"
-import { UserBar } from "../UserBar/UserBar"
+import './styles.css'
 
 export function Header() {
   return (
-    <header className="container-fluid p-0 border-bottom border-1 d-flex justify-content-between align-items-center" style={{height: '120px'}}>
-      <Logo />
-      <SearchBar />
-      <UserBar />
+    <header>
+      <div className="header-container">
+        <div className="logo text-primary">FarmaVita</div>
+        <div className="menu">
+          <img src="./menu-icon.png" alt="menu" />
+        </div>
+      </div>
+      <div className="search">
+        <input type="text" placeholder="O que você está procurando?" />
+        <img src="./search-icon.png" alt="search" />
+      </div>
+      <div className="user">
+        <div className='cart'>
+          <img src="./cart-icon.png" alt="cart" />
+          CARRINHO
+        </div>
+        <div className='account'>
+          <img src="./user-icon.png" alt="user" />
+          ACESSAR CONTA</div>
+      </div>
     </header>
   )
 }
--- src/components/Logo/Logo.tsx
+++
@@ -1,7 +0,0 @@
-export function Logo() {
-  return (
-    <div className="text-primary ms-4 fs-2" style={{fontFamily: "Montserrat Alternates", fontWeight: 600}}>
-      FarmaVita
-    </div>
-  )
-}
